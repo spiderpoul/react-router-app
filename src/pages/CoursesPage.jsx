b6c08e45c69255baa0598a26d9b36dce017be8fc
Pavel Uvarov <Pavel.Uvarov@kaspersky.com>
2023-09-24 15:42:50 +0300

    add error page

--- src/pages/CoursesPage.jsx
+++ src/pages/CoursesPage.jsx
@@ -3,12 +3,14 @@ import { CourseCard } from "../components/CourseCard";
 import { mockFetch } from "../utils/api";
 import { Loader } from "../components/Loader";
 import { useSearchParams } from "react-router-dom";
+import { ErrorPage } from "./ErrorPage";
 
 export const CoursesPage = () => {
   const [data, setData] = useState();
   const [searchParams, setSearchParams] = useSearchParams();
-  const searchFromQuery  = searchParams.get("search");
+  const searchFromQuery = searchParams.get("search");
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
   const [search, setSearch] = useState(searchFromQuery || "");
 
   useEffect(() => {
@@ -16,6 +18,7 @@ export const CoursesPage = () => {
 
     mockFetch("/courses", { search })
       .then((data) => setData(data))
+      .catch((error) => setError(error))
       .finally(() => setIsLoading(false));
 
     setSearchParams((params) => {
@@ -31,6 +34,8 @@ export const CoursesPage = () => {
   if (!data) {
     return <Loader />;
   }
+
+  if (error) return <ErrorPage errorCode={error.status} />;
 
   return (
     <div className="flex-col">
