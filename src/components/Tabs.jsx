1618816d079d1202d543e5c6deb10355793a6256
Pavel Uvarov <Pavel.Uvarov@kaspersky.com>
2023-10-09 11:21:38 +0300

    init workshop 2

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -7,7 +7,6 @@ export const Tabs = ({ tabs }) => {
         <li className="mr-2" key={path}>
           <Link
             to={path}
-            aria-current="page"
             className="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg"
           >
             {title}
--- src/pages/CourseDeatilsPage.jsx
+++ src/pages/CourseDeatilsPage.jsx
@@ -20,11 +20,6 @@ const TABS = [
 
 export const CourseDetails = () => {
   const [data, setData] = useState();
-  const { id } = useParams();
-
-  useEffect(() => {
-    mockFetch(`/courses/${id}`).then(setData);
-  }, [id]);
 
   if (!data) return <Loader />;
 
@@ -44,11 +39,10 @@ export const CourseDetails = () => {
           <div className="content-type">Course</div>
           <div className="course-title">{title}</div>
           <p className="mt-2 text-slate-500 mb-6">{description}</p>
-          <Tabs tabs={TABS} />
-          <Routes>
-            <Route index element={<CourseDescription />} />
-            <Route path="author" element={<CourseAuthor />} />
-          </Routes>
+
+          <CourseDescription />
+          <CourseAuthor />
+
           <LinkButton to="start-course" title="Start course" />
         </div>
       </div>
