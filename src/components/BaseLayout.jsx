30236b3ac0cda3a89961a7f1ce344e3bcb211479
Pavel Uvarov <Pavel.Uvarov@kaspersky.com>
2023-10-09 10:35:56 +0300

    workshop-1

--- src/components/BaseLayout.jsx
+++ src/components/BaseLayout.jsx
@@ -1,12 +1,13 @@
 import { Header } from "./Header";
+import { MainNavigation } from "./MainNavigation";
 
 export const BaseLayout = ({ children }) => {
   return (
     <div className="flex flex-col items-center">
-      <Header>Header</Header>
-      <div className="flex flex-col px-6 relative h-full w-2/3">
-        Main Content
-      </div>
+      <Header>
+        <MainNavigation />
+      </Header>
+      <div className="flex flex-col px-6 relative h-full w-2/3">{children}</div>
     </div>
   );
 };
--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,5 +1,20 @@
+import { Link, NavLink } from "react-router-dom";
 import { NAV_ITEMS } from "../constants";
 
 export const MainNavigation = () => {
-  return <nav className="flex"></nav>;
+  return (
+    <nav className="flex">
+      {NAV_ITEMS.map((item) => (
+        <NavLink
+          key={item.path}
+          to={item.path}
+          className={({ isActive }) =>
+            isActive ? "px-3 text-amber-300" : "px-3"
+          }
+        >
+          {item.title}
+        </NavLink>
+      ))}
+    </nav>
+  );
 };
